f8e687dc89e8d21d7df292121141ec12d8b459c5
Aleksey Kozhevnikov <ak@idamob.ru>
2012-11-02 16:54:11 +0100

    dismiss helpers

---
+++ UIViewController+iOsUtils.h
@@ -0,0 +1,18 @@
+//
+//  UIViewController+iOsUtils.h
+//  Instabank
+//
+//  Created by Aleksey Kozhevnikov on 02.11.12.
+//  Copyright (c) 2012 iDa Mobile. All rights reserved.
+//
+
+@interface UIViewController(iOsUtils)
+
+// If self controller has no presented view controller calls completion block instantly,
+// else the behaviour is the same as dismissViewControllerAnimated:completion:
+-(void)dismissPresentedViewControllerAnimated:(BOOL)flag completion:(void (^)(void))completion;
+
+// Work for navigation controllers and when modally presented
+-(void)dismissSelfAnimated:(BOOL)flag;
+
+@end
---
+++ UIViewController+iOsUtils.m
@@ -0,0 +1,31 @@
+//
+//  UIViewController+iOsUtils.m
+//  Instabank
+//
+//  Created by Aleksey Kozhevnikov on 02.11.12.
+//  Copyright (c) 2012 iDa Mobile. All rights reserved.
+//
+
+#import "UIViewController+iOsUtils.h"
+
+@implementation UIViewController(iOsUtils)
+
+-(void)dismissPresentedViewControllerAnimated:(BOOL)flag completion:(void (^)(void))completion
+{
+  if( !self.presentedViewController ) {
+    completion();
+  } else {
+    [self dismissViewControllerAnimated:flag completion:completion];
+  }
+}
+
+-(void)dismissSelfAnimated:(BOOL)flag
+{
+  if( self.navigationController && self.navigationController.topViewController == self ) {
+    [self.navigationController popViewControllerAnimated:flag];
+  } else if( self.presentingViewController && self.presentingViewController.presentedViewController == self ) {
+    [self dismissViewControllerAnimated:flag completion:^{}];
+  }
+}
+
+@end
